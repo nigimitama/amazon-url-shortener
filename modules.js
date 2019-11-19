180c85e4ead7a0358475066d45eb98bb186406af
nigimitama <nigimitama16@gmail.com>
2019-11-19 22:24:55 +0900

    htmlElementをjsで追加したり削除したりするのではなく、構造はhtmlで静的に保持し、htmlElementのhidden属性をjsで更新するように変更

--- modules.js
+++ modules.js
@@ -1,27 +1,11 @@
 function runShortener() {
-  removeOutput();
+  changeVisibility("resultArea", "hidden");
+  changeVisibility("messageArea", "hidden");
   var url = document.forms.inputForm.textBox.value;
   if ((url === null) || (url === '')) {
-    notifyError('Please Enter Url(s) of Amazon.com');
+    showMessage('Please Enter Url(s) of Amazon.com');
   } else {
     writeOutput();
-  }
-}
-
-function removeOutput() {
-  var target = document.getElementById('output');
-  if (target !== null) {
-    target.remove();
-    var copyButton = document.getElementById('copyButton');
-    copyButton.remove();
-    var notifyCopied = document.getElementById('notifyCopied');
-    if (notifyCopied !== null) {
-      notifyCopied.remove();
-    }
-  }
-  var notifyError = document.getElementById('notifyError');
-  if (notifyError !== null) {
-    notifyError.remove();
   }
 }
 
@@ -29,59 +13,29 @@ function writeOutput() {
   const url = document.forms.inputForm.textBox.value;
   const shortUrl = shortenUrls(url);
   if ((shortUrl === null) || (shortUrl === '')) {
-    notifyError('Error: wrong input');
+    showMessage('Error: wrong input');
   } else {
-    target = createOutputTarget();
+    changeVisibility("resultArea", "show");
+    var target = document.getElementById('result');
     target.value = shortUrl;
-    createCopyButton();
   }
 }
 
-function createOutputTarget() {
-  var target = document.createElement('textarea');
-  target.setAttribute('id', 'output');
-  target.setAttribute('style', 'resize: both;');
-  target.classList.add('form-control');
-  var outputArea = document.getElementById("outputArea");
-  outputArea.appendChild(target);
-  return target;
-}
-
-function createCopyButton() {
-  var button = document.createElement('button');
-  button.setAttribute('onclick', 'copyToClipboard()');
-  button.setAttribute('id', 'copyButton')
-  button.classList.add('btn');
-  button.classList.add('btn-light');
-  button.classList.add('my-1');
-  button.innerText = 'copy';
-  var outputArea = document.getElementById("outputArea");
-  outputArea.appendChild(button);
-}
-
 function copyToClipboard() {
-  var copyTarget = document.getElementById("output");
-  copyTarget.select();
+  var result = document.getElementById("result");
+  result.select();
   document.execCommand("copy");
-  // alert("copied: " + copyTarget.value);
-  notifyCopied();
+  // alert("copied: " + result.value);
+  showMessage("copied!");
 }
 
-function notifyCopied() {
-  var outputArea = document.getElementById("outputArea");
-  var p = document.createElement('p');
-  p.setAttribute('id', 'notifyCopied');
-  outputArea.appendChild(p);
-  p.innerText = 'copied!';
+function showMessage(message) {
+  changeVisibility("messageArea", "show");
+  var p = document.getElementById("message");
+  p.innerHTML = message;
 }
 
-
-function notifyError(message) {
-  var outputArea = document.getElementById("outputArea");
-  var p = document.createElement('p');
-  p.setAttribute('id', 'notifyError');
-  outputArea.appendChild(p);
-  p.innerText = message;
+function changeVisibility(id, value) {
+  var element = document.getElementById(id);
+  element.hidden = (value === "show") ? false : true;
 }
-
-
